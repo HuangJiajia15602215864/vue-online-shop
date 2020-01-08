8b0b3c7e7ed02274d58b807930b16df0a8dad2af
@huangjj <huangjj@minstone.com.cn>
2020-01-08 10:12:28 +0800

    提交电商平台Vue页面构建代码。

---
+++ vue-online-shop-frontend/src/pages/admin/Index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <div class="admin-new">
+      <div class="container">
+        <div class="col-lg-3 col-md-3 col-sm-12 col-xs-12">
+          <ul class="admin-menu">
+            <li>
+              <router-link to="/admin">View Products</router-link>
+            </li>
+            <li>
+              <router-link to="/admin/new">New Products</router-link>
+            </li>
+          </ul>
+        </div>
+        <!-- router-link 导向更深层级的路由 -->
+        <router-view></router-view>
+      </div>
+    </div>
+  </div>
+</template>
